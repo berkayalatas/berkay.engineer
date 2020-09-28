d2a9c97fb68d3813e933d347a7647979bcb63eee
berkayalatas <berkayalatas1997@gmail.com>
2020-09-28 18:30:12 +0300

    Last Update

--- js/script.js
+++ js/script.js
@@ -1,174 +1,176 @@
  
-const spans = document.querySelectorAll('h1 span');
-const spans2 = document.querySelectorAll('h2 span');
+	const spans = document.querySelectorAll('h1 span');
 
+	//greeting 
+	spans.forEach(span => span.addEventListener('mouseover', function (e) {
+	    span.classList.add('animated', 'rubberBand')
+	}))
 
-//greeting 
-spans.forEach(span => span.addEventListener('mouseover', function (e) {
-    span.classList.add('animated', 'rubberBand')
-}))
-
-spans.forEach(span => span.addEventListener('mouseout', function (e) {
-    span.classList.remove('animated', 'rubberBand')
-}))
-
-//about me text //myskills text
-spans2.forEach(span => span.addEventListener('mouseover', function (e) {
-    span.classList.add('animated', 'rubberBand')
-}))
-
-spans2.forEach(span => span.addEventListener('mouseout', function (e) {
-    span.classList.remove('animated', 'rubberBand')
-}))
-
-
-/* Bar icon for responsive website */
-$(".nav-icon").click(function(){
-    $("#nav__container").toggleClass("responsive");
-    return false
-})
+	spans.forEach(span => span.addEventListener('mouseout', function (e) {
+	    span.classList.remove('animated', 'rubberBand')
+	}))
 
 
 
-/* navbar */
-var ww = $(window).width();
-
-if (ww > 500) {
-
-    $(window).scroll(function () {
-
-        if ($(window).scrollTop() < 100)
-            $("header").css({
-                "background-color": "rgba(0,0,0,.5)"
-            });
-
-        else
-            $("header").css({
-                "background-color": "rgb(15, 14, 14)",
-            });
-    });
-
-
-}
-
-
-// bars mouseover
-$("section .p-85").mouseover(function () {
-    $("#css").hide();
-})
-
-
-$("section .p-85").mouseleave(function () {
-    $("#css").show();
-})
-
-
-$("section .p-95").mouseover(function () {
-    $("#html").hide();
-})
-
-
-$("section .p-95").mouseleave(function () {
-    $("#html").show();
-})
+	/* Bar icon for responsive website */
+	$(".nav-icon").click(function () {
+	    $("#nav__container").toggleClass("responsive");
+	    return false
+	})
 
 
 
-$("section .p-80").mouseover(function () {
-    $("#bootstrap").hide();
-})
+	/* navbar */
+	var ww = $(window).width();
+
+	if (ww > 500) {
+
+	    $(window).scroll(function () {
+
+	        if ($(window).scrollTop() < 100)
+	            $("header").css({
+	                "background-color": "rgba(0,0,0,.5)"
+	            });
+
+	        else
+	            $("header").css({
+	                "background-color": "rgb(15, 14, 14)",
+	            });
+	    });
 
 
-$("section .p-80").mouseleave(function () {
-    $("#bootstrap").show();
-})
+	}
 
 
-
-$("section .p-75").mouseover(function () {
-    $("#javascript").hide();
-})
-
-
-$("section .p-75").mouseleave(function () {
-    $("#javascript").show();
-})
+	// bars mouseover
+	$("section .p-85").mouseover(function () {
+	    $("#css").hide();
+	})
 
 
-$("section #brown").mouseover(function () {
-    $("#jquery").hide();
-})
+	$("section .p-85").mouseleave(function () {
+	    $("#css").show();
+	})
 
 
-$("section #brown").mouseleave(function () {
-    $("#jquery").show();
-})
+	$("section .p-95").mouseover(function () {
+	    $("#html").hide();
+	})
 
 
-$("section #brown2").mouseover(function () {
-    $("#python").hide();
-})
-
-
-$("section #brown2").mouseleave(function () {
-    $("#python").show();
-})
-
-
-$("section .p-50").mouseover(function () {
-    $("#django").hide();
-})
-
-
-$("section .p-50").mouseleave(function () {
-    $("#django").show();
-})
-// bars mouseover end
+	$("section .p-95").mouseleave(function () {
+	    $("#html").show();
+	})
 
 
 
-//to top button
-mybutton = document.getElementById("myBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
-//enf of the top button
+	$("section .p-80").mouseover(function () {
+	    $("#bootstrap").hide();
+	})
 
 
-/* Contact with Email*/
-$('#alert').hide()
+	$("section .p-80").mouseleave(function () {
+	    $("#bootstrap").show();
+	})
 
-const name = $('#fullname').val()
-const email = $('#email').val()
-const textMessage = $('#textarea').val()
-const form = $('#form')
-const alert = $('#alert')
 
-form.submit(function generateEmail(e){
+
+	$("section .p-75").mouseover(function () {
+	    $("#javascript").hide();
+	})
+
+
+	$("section .p-75").mouseleave(function () {
+	    $("#javascript").show();
+	})
+
+
+	$("section #brown").mouseover(function () {
+	    $("#jquery").hide();
+	})
+
+
+	$("section #brown").mouseleave(function () {
+	    $("#jquery").show();
+	})
+
+
+	$("section #brown2").mouseover(function () {
+	    $("#python").hide();
+	})
+
+
+	$("section #brown2").mouseleave(function () {
+	    $("#python").show();
+	})
+
+
+	$("section .p-50").mouseover(function () {
+	    $("#django").hide();
+	})
+
+
+	$("section .p-50").mouseleave(function () {
+	    $("#django").show();
+	})
+	// bars mouseover end
+
+
+
+    //to top button
+	mybutton = document.getElementById("myBtn");
+
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function () {
+	    scrollFunction()
+	};
+
+	function scrollFunction() {
+	    if (document.body.scrollTop > 700 || document.documentElement.scrollTop > 700) {
+	        mybutton.style.display = "block";
+	    } else {
+	        mybutton.style.display = "none";
+	    }
+	}
+
+	function topFunction() {
+	    document.body.scrollTop = 0; // For Safari
+	    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+	//enf of the top button
+
     
-    var emailTo = "berkay3304@gmail.com";
-    var emailBody = `Message:  ${$('#textarea').val()}  Contact:  ${$('#email').val()}`; 
-    var emailSubject = "Work " + $('#fullname').val() 
-    location.href = "mailto:" + emailTo + "?" + 
-        (emailBody ? "&body=" + emailBody : "") + 
-        (emailSubject ? "&subject=" + emailSubject : "");
 
-    e.preventDefault()
- 
-})
+	/* Contact with Email*/
+	$('#alert').hide()
+
+	const name = $('#fullname').val()
+	const email = $('#email').val()
+	const textMessage = $('#textarea').val()
+	const form = $('#form')
+	const alert = $('#alert')
+
+	form.submit(function generateEmail(e) {
+
+	    var emailTo = "berkay3304@gmail.com";
+	    var emailBody = `Message:  ${$('#textarea').val()}  Contact:  ${$('#email').val()}`;
+	    var emailSubject = "Work " + $('#fullname').val()
+	    location.href = "mailto:" + emailTo + "?" +
+	        (emailBody ? "&body=" + emailBody : "") +
+	        (emailSubject ? "&subject=" + emailSubject : "");
+
+	    e.preventDefault()
+
+	})
 
 
+    /* animation 
+
+    $("nav a[href*='#']").click(function() { 
+    
+        $("html, body").animate({
+            scrollTop: $($(this).attr("href")).offset().top -100;
+        },200 /* speed   );
+    });
+  */
 
